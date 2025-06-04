5909c8e60aa84e9907be9614d4f43ce151097233
ahirota <avjhirota@gmail.com>
2025-06-03 21:53:58 -0700

    [ASTEROIDS_3_1] Implement Asteroid Field Spawn system

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -9,10 +11,14 @@ def main():
     dt = 0
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
     player = Player(x, y)
+    asteroidField = AsteroidField()
 
     while(True):
         for event in pygame.event.get():
